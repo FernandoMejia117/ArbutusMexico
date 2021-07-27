a063d076cd61303e36100f9869f96827926a3cb3
Octivuz <84882221+Octivuz@users.noreply.github.com>
2021-07-26 19:52:22 -0500

    ABC
    Ya funciona el CRUD

--- server/routes/madroños.routes.js
+++ server/routes/madroños.routes.js
@@ -4,5 +4,8 @@ const Controladormadroño =  require('../controller/madroños.controller')
 
 router.get('/',Controladormadroño.indexMadroño)
       .post('/',Controladormadroño.crearMadroño)
+      .get('/:key/:value',Controladormadroño.buscarMadroño,Controladormadroño.mostrarMadroño)
+      .put('/:key/:value',Controladormadroño.buscarMadroño,Controladormadroño.actualizarMadroño)
+      .delete('/:key/:value',Controladormadroño.buscarMadroño,Controladormadroño.eliminarMadroño)
 
 module.exports = router;
